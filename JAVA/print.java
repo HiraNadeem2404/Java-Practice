695911e954c690f1279f907d1f7d33a74bd40483
Hira <hirarani2404@gmail.com>
2024-08-12 11:17:40 +0500

    First commit -devB

---
+++ JAVA/print.java
@@ -0,0 +1,8 @@
+public class print {
+    public static void main(String a[]) {
+        System.out.print("hello");
+        System.out.print("123");
+        System.out.print("");
+        System.out.print("456");
+    }
+}
---
+++ JAVA/typeconversion.java
@@ -0,0 +1,11 @@
+public class typeconversion{
+    public static void main(String[] args) {
+        int a=12;
+        byte b=127;
+        System.out.println(a);
+        System.out.println(b);
+        b=(byte)a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
